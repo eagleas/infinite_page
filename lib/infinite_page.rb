77d5fa3fa8aa7994f200ffe37107476c26937547
Alex Eagle <eagle.alex@gmail.com>
2010-11-25 11:49:12 +0300

    Ruby 1.9 syntax and rails3 compatibility commit

--- lib/infinite_page.rb
+++ lib/infinite_page.rb
@@ -1,48 +1,49 @@
-include WillPaginate
-include ActionView::Helpers::PrototypeHelper
-include ActionView::Helpers::JavaScriptHelper
-include ActionView::Helpers::TagHelper
-
+require 'will_paginate/view_helpers/action_view'
 
 module InfinitePage
+
+  include WillPaginate::ViewHelpers::ActionView
+  include ActionView::Helpers::PrototypeHelper
+  include ActionView::Helpers::JavaScriptHelper
+  include ActionView::Helpers::TagHelper
+
   def infinite_page(collection = nil, options = {})
     unless WillPaginate::ViewHelpers.total_pages_for_collection(collection) <= 1
       options[:renderer] = 'InfinitePage::ScrollObserverRenderer'
       pagination = will_paginate(collection, options)
-      options[:renderer] = 'WillPaginate::LinkRenderer'
+      options[:renderer] = 'WillPaginate::ViewHelpers::LinkRenderer'
       pagination_nojs = will_paginate(collection, options)
       pagination += "<noscript>"+pagination_nojs+"</noscript>"  unless pagination_nojs.nil?
     else
       js_string = ''
-      js_string += options[:first]+";" unless  options[:first].nil? 
-      js_string += options[:last] unless  options[:last].nil? 
+      js_string += options[:first]+";" unless  options[:first].nil?
+      js_string += options[:last] unless  options[:last].nil?
       pagination = javascript_tag js_string unless  js_string.blank?
     end
   end
-  
-  class ScrollObserverRenderer < WillPaginate::LinkRenderer    
+
+  class ScrollObserverRenderer < WillPaginate::ViewHelpers::LinkRenderer
     def to_html
       if @collection.current_page<@collection.total_pages
         @options[:url] = @collection.next_page
-        @options[:method] = :get if @options[:method].nil?              
-        js_string = "InfinitePage.start_scroll_observing('"+escape_javascript(remote_function @options)+"');"      
+        @options[:method] = :get if @options[:method].nil?
+        js_string = "InfinitePage.start_scroll_observing('"+escape_javascript(remote_function @options)+"');"
         if @collection.current_page==1
           js_string += @options[:first] unless  @options[:first].nil?
         end
         js_string = javascript_tag js_string
       else
-        javascript_tag @options[:last] unless  @options[:last].nil?              
+        javascript_tag @options[:last] unless  @options[:last].nil?
       end
     end
-    
+
     def protect_against_forgery?
       config = Rails::Configuration.new
       config.action_controller.allow_forgery_protection
     end
-  end 
+  end
 end
 
 if defined?(Rails) and defined?(ActionController)
-  return if ActionView::Base.instance_methods.include? 'infinite_page'
-  ActionView::Base.send :include, InfinitePage
+  ActionView::Base.send :include, InfinitePage unless ActionView::Base.instance_methods.include? 'infinite_page'
 end
